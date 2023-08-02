a625a5dc098daf2645f59b3799d34f05a9d2dfd2
akr9757 <anilkumareddy9757@gmail.com>
2023-08-02 20:02:21 +0530

    parameter store

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "aws_ssm_parameter" "parameter" {
   name  = "akr.akr"
-  type  = "String"
+  type  = "SecureString"
   value = "how r u"
+  key_id = "7324a4aa-43b2-47be-98cf-a76f8acbc746"
 }
