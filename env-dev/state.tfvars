05bbfa5b478a77b623621913e4f04b5b0063bcdd
akr9757 <anilkumareddy9757@gmail.com>
2023-09-17 16:58:06 +0530

    grok expression

--- env-dev/state.tfvars
+++ env-dev/state.tfvars
@@ -1,3 +1,3 @@
-bucket = "akr-terraform"
+bucket = "akr9757-bucket"
 key    = "parameter-store/dev/terraform.tfstate"
 region = "us-east-1"
--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_ssm_parameter" "parameters" {
   name  = var.parameters[count.index].name
   type  = "String"
   value = var.parameters[count.index].value
-  key_id = "7324a4aa-43b2-47be-98cf-a76f8acbc746"
+  key_id = "5816e42f-a81c-499f-b64a-cfe36d3610ba"
 }
 
 resource "aws_ssm_parameter" "passwords" {
@@ -11,5 +11,5 @@ resource "aws_ssm_parameter" "passwords" {
   name  = var.passwords[count.index].name
   type  = "SecureString"
   value = var.passwords[count.index].value
-  key_id = "7324a4aa-43b2-47be-98cf-a76f8acbc746"
+  key_id = "5816e42f-a81c-499f-b64a-cfe36d3610ba"
 }
